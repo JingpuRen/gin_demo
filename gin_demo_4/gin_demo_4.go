59a8472e6d09afbf00d5e22acccbc7def77aec4d
JingpuRen <yy5396681@qq.com>
2025-02-15 22:39:47 +0800

    update

--- gin_demo_4/gin_demo_4.go
+++ gin_demo_4/gin_demo_4.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+// todo 通过gin框架实现文件上传
 
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/web", func(context *gin.Context) {
+
+	})
+
+	r.Run("localhost:8080")
 }
