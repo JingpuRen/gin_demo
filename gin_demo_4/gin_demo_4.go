61a1bfbbf7fd761f58cbe84ca6b3daf72fd563ce
JingpuRen <yy5396681@qq.com>
2025-02-15 23:10:28 +0800

    update

--- gin_demo_4/gin_demo_4.go
+++ gin_demo_4/gin_demo_4.go
@@ -1,6 +1,6 @@
 package main
 
-// todo 通过gin框架实现http重定向和路由重定向
+// tip 通过gin框架实现http重定向和路由重定向
 
 import (
 	"github.com/gin-gonic/gin"
@@ -10,12 +10,12 @@ import (
 func main() {
 	router := gin.Default()
 
-	// todo http请求重定向
+	// tip http请求重定向
 	router.GET("/index", func(context *gin.Context) {
 		context.Redirect(http.StatusMovedPermanently, "http://www.sogo.com/")
 	})
 
-	// todo 内部路由重定向
+	// tip 内部路由重定向
 	router.GET("/web1", func(context *gin.Context) {
 		context.Request.URL.Path = "/web2" // 修改请求中的URL信息
 		router.HandleContext(context)      // 继续后续的处理
