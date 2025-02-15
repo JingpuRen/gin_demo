1c027880e2531940d169f4421f1032c4ca3e3ff1
JingpuRen <yy5396681@qq.com>
2025-02-15 22:55:15 +0800

    update

--- gin_demo_4/gin_demo_4.go
+++ gin_demo_4/gin_demo_4.go
@@ -1,15 +1,30 @@
 package main
 
-// todo 通过gin框架实现文件上传
+// todo 通过gin框架实现http重定向和路由重定向
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
-	r.GET("/web", func(context *gin.Context) {
-
+	// todo http请求重定向
+	router.GET("/index", func(context *gin.Context) {
+		context.Redirect(http.StatusMovedPermanently, "http://www.sogo.com/")
 	})
 
-	r.Run("localhost:8080")
+	// todo 内部路由重定向
+	router.GET("/web1", func(context *gin.Context) {
+		context.Request.URL.Path = "/web2" // 修改请求中的URL信息
+		router.HandleContext(context)      // 继续后续的处理
+	})
+	router.GET("./web2", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"data": "you are succeed",
+		})
+	})
+
+	router.Run("localhost:8080")
 }
